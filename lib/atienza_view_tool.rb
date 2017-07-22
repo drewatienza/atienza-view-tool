cbd444d1c976df8586f405cc328e48898885785b
drewatienza <atienza.drew@gmail.com>
2017-07-22 11:45:39 -0700

    Atienza view tool initial commit

---
+++ lib/atienza_view_tool.rb
@@ -0,0 +1,6 @@
+require "atienza_view_tool/version"
+require "atienza_view_tool/remderer"
+
+module AtienzaViewTool
+  # Your code goes here...
+end
---
+++ lib/atienza_view_tool/version.rb
@@ -0,0 +1,3 @@
+module AtienzaViewTool
+  VERSION = "0.1.0"
+end
